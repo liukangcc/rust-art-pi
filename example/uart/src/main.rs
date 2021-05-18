40781a2e84aa8f51f2858f44c22a04ef41261976
Henson <guozhanxin@rt-thread.com>
2021-05-18 23:10:11 +0800

    优化代码

--- example/uart/src/main.rs
+++ example/uart/src/main.rs
@@ -39,12 +39,9 @@ fn main() -> ! {
     let tx = gpioa.pa0.into_alternate_af8();
     let rx = gpioi.pi9.into_alternate_af8();
 
-    let mut uart_config = serial::config::Config::default();
-    uart_config.baudrate = 115_200_u32.bps();
-
     let mut serial = dp
         .UART4
-        .usart((tx, rx), uart_config, &mut ccdr)
+        .usart((tx, rx), serial::config::Config::default().baudrate(115_200_u32.bps()), &mut ccdr)
         .unwrap();
 
     serial.listen(serial::Event::Rxne);
